767039de4c464309b1ad298a472b768fc43bbb3d
Naufal Asad Alhaq <asadalhaq@yandex.com>
2024-01-07 14:55:48 +0700

    feat: log query

--- book-catalog-db/src/lib/book-catalog-db.service.ts
+++ book-catalog-db/src/lib/book-catalog-db.service.ts
@@ -3,6 +3,12 @@ import { PrismaClient } from './generated';
 
 @Injectable()
 export class BookCatalogDBService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ['query'],
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
